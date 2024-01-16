15f27e078e3613fe583572ff98825fb4991352ec
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2024-01-16 16:22:31 +0100

    change: reload page when have any update

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import App from './App'
 import './index.scss'
+import { message } from 'antd'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,9 +11,11 @@ root.render(<App />)
 
 serviceWorkerRegistration.register({
   onUpdate: (registration: any) => {
+    const reload = () => window.location.reload();
     if (registration && registration.waiting) {
-      registration.waiting.postMessage({ type: "SKIP_WAITING" })
-      registration.update()
+      registration.waiting.postMessage({ type: "SKIP_WAITING" });
+      registration.update();
+      message.info("Nueva versión. Actualizando la web", reload);
     }
   }
 })
