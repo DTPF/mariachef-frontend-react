83c4179b8581bd19e5d1211060396a89c68e96b6
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2024-01-16 20:20:22 +0100

    feat: app route variables

---
+++ src/router/paths.ts
@@ -0,0 +1,12 @@
+export const routes = {
+  home: '/',
+  myRecipes: '/my-recipes',
+  favorites: '/favorites',
+  categories: '/categories',
+  search: '/search',
+  // Admin
+  admin: '/admin',
+  adminHome: '/admin/home',
+  adminContent: '/admin/content',
+  adminUsers: '/admin/users',
+}
